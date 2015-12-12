12a2688048f2e6c74e5cb2b8ba6425572e24f73e
Dave Dittrich <dittrich@u.washington.edu>
2015-12-11 17:45:52 -0800

    Make Tupelo docs/source/conf.py be like all the rest

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,8 @@ extensions = [
     'sphinx.ext.ifconfig',
 ]
 
+todo_include_todos = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -45,7 +47,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
@@ -128,12 +130,12 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'UW-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'UW-logo-32x32.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -237,7 +239,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'UW-logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
